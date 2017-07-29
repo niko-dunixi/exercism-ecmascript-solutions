d12db0adb8f443169dbdb997206bd261ee063d80
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-29 03:07:26 -0600

    Passing all tests

--- leap/leap.js
+++ leap/leap.js
@@ -1,11 +1,14 @@
 class Year {
   constructor(year) {
-    this.y = year;
+    this.year = year;
   }
 
   isLeap() {
-    console.log(this.year);
-    return this.year % 4 == 0; // && (this.year % 100 != 0 || this.year % 400 == 0);
+    var y = this.year;
+    var divisibleByFour = y % 4 == 0;
+    var divisibleByOneHundred = y % 100 == 0;
+    var divisibleByFourHundred = y % 400 == 0;
+    return divisibleByFour && (!divisibleByOneHundred || divisibleByFourHundred);
   }
 }
 
