01fdb47bb3073a3cd3e3ccce633c17683b28f9c5
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-29 03:15:39 -0600

    First implementation
    Passes the first test by iterating over both strings and incrementing when elements differ.

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,7 +1,16 @@
 class Hamming {
 
   compute(a, b) {
-    return -1;
+    var distance = 0;
+
+    for (var i = 0, length = a.length; i < length; i++) {
+      var first = a.charAt(i);
+      var second = b.charAt(i);
+      if (first != second) {
+        distance++;
+      }
+    }
+    return distance;
   }
 
 }
