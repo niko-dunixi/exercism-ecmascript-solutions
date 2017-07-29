1bf7f92106e581abec832d8c541ac4a6b9c9616f
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-29 03:33:56 -0600

    pass first test

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,6 +1,19 @@
 class Transcriptor {
-  toRna(rna) {
-    return '';
+  toRna(dna) {
+    let rna = '';
+    for (let i = 0, length = dna.length; i < length; i++) {
+      let current = dna.charAt(i);
+      if (current === 'G') {
+        rna += 'C';
+      } else if (current === 'C') {
+        rna += 'G';
+      } else if (current === 'T') {
+        rna += 'A';
+      } else if (current === 'A') {
+        rna += 'U';
+      }
+    }
+    return rna;
   }
 }
 
