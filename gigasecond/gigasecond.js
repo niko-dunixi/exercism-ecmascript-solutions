1700d33087305f1a74ab074d307ed2124dbe21b1
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-30 02:33:52 -0600

    Passes first test
    This wasn't easy to do. Date is really jenky in javascript. In order to
    do date manipulation we need to work in milliseconds and with the Date
    constructor. It's the ONLY way I've see work.

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,6 +1,16 @@
-class Gigasecond {
+const gigasecond = (10 ** 9) * 1000; // gigaseconds converted to ms
 
-  date() {}
+class Gigasecond {
+  constructor(date) {
+    // This is super un-intuitive. Especially because every other
+    // resource shows that setSecond should work just fine.
+    // TL;DR: it doesn't :/
+    this.futureDate = new Date(date.getTime() + gigasecond);
+  }
+
+  date() {
+    return this.futureDate;
+  }
 }
 
 export default Gigasecond;
