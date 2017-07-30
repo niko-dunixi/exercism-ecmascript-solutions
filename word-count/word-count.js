dc3985f80c0a2529917f8d2e4a0cbcec2c7d3893
Paul Baker <paul.baker@wgu.edu>
2017-07-30 03:18:20 -0600

    Passes test

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,6 +1,15 @@
 class Words {
   count(phrase) {
-    return {};
+    let words = phrase.split(/\s+/)
+    let result = {};
+    for (let word of words) {
+      if (result[word]) {
+        result[word]++;
+      } else {
+        result[word] = 1;
+      }
+    }
+    return result;
   }
 }
 
