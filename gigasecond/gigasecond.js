382eb2a6f70e012b0533f95666eb9b284e9c8e41
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-30 02:41:29 -0600

    All tests pass

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,11 +1,11 @@
-const gigasecond = (10 ** 9) * 1000; // gigaseconds converted to ms
+const gsAsMs = (10 ** 9) * 1000; // gigaseconds converted to ms
 
 class Gigasecond {
   constructor(date) {
     // This is super un-intuitive. Especially because every other
     // resource shows that setSecond should work just fine.
     // TL;DR: it doesn't :/
-    this.futureDate = new Date(date.getTime() + gigasecond);
+    this.futureDate = new Date(date.getTime() + gsAsMs);
   }
 
   date() {
