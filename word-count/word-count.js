28682aadc83978f2e02727fac47a27e150ff5bcc
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-30 03:51:33 -0600

    Solved it...
    This isn't a solution I like. I don't like it, because it doesn't make
    sense and seems arbitrary. TODO: research
    
    https://mcculloughwebservices.com/2015/05/12/exercism-word-count/
    http://www.htmlgoodies.com/beyond/javascript/object.create-the-new-way-to-create-objects-in-javascript.html

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,7 +1,13 @@
 class Words {
   count(phrase) {
     let words = phrase.trim().toLowerCase().split(/\s+/)
-    let result = {};
+
+    // We can't create a dict literal with curly brackets.
+    // This results in a NaN for the string 'constructor'.
+    // I don't know why, but this solution works.
+    // https://mcculloughwebservices.com/2015/05/12/exercism-word-count/
+    let result = Object.create(null);
+
     for (let word of words) {
       if (word in result) {
         result[word]++;
